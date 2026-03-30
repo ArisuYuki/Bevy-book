b1b255d3ce35da40bfc8818e1f5c1b1b9ab79f54
Shiraha Yuki <shirahayuki2002@gmail.com>
2026-03-30 18:01:47 +0800

    Update quaternion

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-85f544ed.js',
+                path_to_root + 'searchindex-ef5b4a70.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
