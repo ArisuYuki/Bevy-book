3276fa97cb8449b2041da418142103eec1ba9283
Shiraha Yuki <shirahayuki2002@gmail.com>
2026-04-03 20:32:28 +0800

    refactor: Change chapter structure and order

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ca1f233b.js',
+                path_to_root + 'searchindex-ee7819df.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
