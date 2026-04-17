107d987eeac5f6cb11380540ae591548c417c51c
Shiraha Yuki <shirahayuki2002@gmail.com>
2026-04-17 12:51:01 +0800

    feat&refactor:  refactoring Chapter 7 and adding a more detailed introduction to gltf

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2811c7ac.js',
+                path_to_root + 'searchindex-f53c9da3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
