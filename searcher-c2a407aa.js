98828b9eb66e7ddd4244eb8efaa26183d691433b
Shiraha Yuki <shirahayuki2002@gmail.com>
2026-05-11 17:00:51 +0800

    update 7.2 camera2d and adjust some language

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f6872c50.js',
+                path_to_root + 'searchindex-477a0cf4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
